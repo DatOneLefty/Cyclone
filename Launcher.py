b971dc6ab80dfb73de8e2945bed9d0414e0e84a1
DatOneLefty <k9dogs101@gmail.com>
2015-11-01 19:54:23 -0500

    setup + basic launcher + interpreter init

---
+++ Launcher.py
@@ -0,0 +1,13 @@
+"""
+Cyclone programming language
+by DatOneLefty
+"""
+print """
+    Cyclone Programming language Pre-Alpha Interpreter
+    Commit 2
+    """
+import intrepeter as inter
+inter.init()
+while True:
+    codeline = raw_input (">>>")
+    
---
+++ intrepeter.py
@@ -0,0 +1,17 @@
+
+def init():
+    print """
+    Cyclone Programming language Intrepeter Pre-Aplha
+    commit 2
+    """
+    num_in_line = 1
+    line = 0
+    num = 0
+    command = 0
+    cmd_data = 0
+def seperate():
+    
+    if num == " ":
+        pass
+    else:
+        pass
