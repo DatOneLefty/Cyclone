4244c090765a90d522ddf2ace653cb7cbdbd6f89
DatOneLefty <k9dogs101@gmail.com>
2015-11-15 14:59:11 -0500

    it works! + bug
    not single line print

--- interpreterfle.py
+++ interpreterfle.py
@@ -5,21 +5,20 @@ var = raw_input ("RUN>")
 def dispcmd(lines):
     if lines == """]
 """:
-        print savedc
         sysp = "null"
     else:
-        savedc = lines
-        sysp = "txtp"
+        if lines != """display [
+""":
+            print lines
+            sysp = "txtp"
 
         
 with open(var) as openfileobject:
     for line in openfileobject:
         if line == """display [
 """:
-            print "LINEF"
             dispcmd("")
         if line == line:
-            print "LINERP"
             dispcmd(line)
 
 
